b8f40edccf070b10137e78b33b74a38a542d4f79
tomy08 <tomasesantacruz@gmail.com>
2025-03-27 20:11:06 -0300

    feat: add exercise 1

---
+++ ej1-tp2-tsc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int main(){
+	string nombre;
+	cout << "Ingresar un nombre: ";
+	cin >> nombre;
+	cout << "El nombre es "<< nombre;
+	return 0;
+}
