a84782008491a4d918206224386fed5984ef1145
tomy08 <tomasesantacruz@gmail.com>
2025-04-02 10:13:37 -0300

    feat: refactor main function to split and reverse input string

--- ej6-tp2-tsc.cpp
+++ ej6-tp2-tsc.cpp
@@ -4,20 +4,37 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     string cadena;
     cout << "Ingresar cadena: ";
-    getline(cin,cadena);
+    getline(cin, cadena);
     cout << endl;
-    string cadena_invertida;
+    vector<string> cadena_split;
+    int start = 0;
 
-    for (int i = cadena.size() - 1; i >= 0; i--)
+    for (int i = 0; i < cadena.length(); i++)
     {
-        cadena_invertida.push_back(cadena[i]);
+        if (cadena[i] == ' ' || cadena[i] == '\t' || cadena[i] == '\n')
+        {
+            if (start < i)
+            {
+                cadena_split.push_back(cadena.substr(start, i - start));
+            }
+            start = i + 1;
+        }
+        else if (i == cadena.length() - 1)
+        {
+            cadena_split.push_back(cadena.substr(start, i - start + 1));
+        }
     }
 
-    cout << cadena_invertida;    
+    for (int i = cadena_split.size() - 1; i >= 0; i--)
+    {
+        cout << cadena_split[i] << ' ';
+    }
+    cout << endl;
 
-
+    system("pause");
     return 0;
 }
