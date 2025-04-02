0f92c720601675a337d1d28906e8027a2a329957
tomy08 <tomasesantacruz@gmail.com>
2025-04-02 10:23:38 -0300

    feat: implement function to find the longest common substring from user input

--- ej8-tp2-tsc.cpp
+++ ej8-tp2-tsc.cpp
@@ -1,57 +1,75 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+bool estaLaSubcadena(const string &sub, const vector<string> &palabras)
+{
+	for (int i = 0; i < palabras.size(); i++)
+	{
+		if (palabras[i].find(sub) == string::npos)
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
-int main(){
-	string palabra1,palabra2,palabra3,palabra4,palabra5;
-	vector<string> prefijos;
-	
+string encontrarSubcadenaMasLarga(vector<string> &palabras)
+{
+	string base = palabras[0];
+	int masLongitud = 0;
+	string subcadenMasLarga;
+
+	for (int i = 1; i <= base.size(); ++i) // controla la longitud de las subcadenas
+	{
+		for (int j = 0; j + i <= base.size(); ++j) // controla la posicion inicial desde donde se extrae la subcadena
+		{
+			string sub = base.substr(j, i); // extrae la subcadena de longitud i desde la posicion j
+			/*
+			Por ejemplo, si la palabra base es "abc", las subcadenas generadas serían:
+			Longitud 1: "a", "b", "c".
+			Longitud 2: "ab", "bc".
+			Longitud 3: "abc".
+			*/
+			if (estaLaSubcadena(sub, palabras) && sub.size() > masLongitud)
+			{
+				masLongitud = sub.size();
+				subcadenMasLarga = sub;
+			}
+		}
+	}
+	return subcadenMasLarga;
+}
+
+int main()
+{
+	vector<string> palabras(5);
+
 	cout << "Ingresar la primer palabra: ";
-	cin >> palabra1;
+	cin >> palabras[0];
 	cout << endl;
-	
+
 	cout << "Ingresar la segunda palabra: ";
-	cin >> palabra2;
+	cin >> palabras[1];
 	cout << endl;
-	
+
 	cout << "Ingresar la tercer palabra: ";
-	cin >> palabra3;
+	cin >> palabras[2];
 	cout << endl;
-	
+
 	cout << "Ingresar la cuarta palabra: ";
-	cin >> palabra4;
+	cin >> palabras[3];
 	cout << endl;
-	
+
 	cout << "Ingresar la quinta palabra: ";
-	cin >> palabra5;
+	cin >> palabras[4];
 	cout << endl;
-	
-	int cantPrefijos = 0;
 
-for(int i = 0; i < palabra1.size(); i++){
-	if (palabra1[i] == palabra2[i] && palabra1[i] == palabra3[i] && palabra1[i] == palabra4[i] && palabra1[i] == palabra5[i]){
-		if (prefijos.size() <= cantPrefijos) {
-    		prefijos.push_back(""); 
-		}
-		prefijos[cantPrefijos].push_back(palabra1[i]);
+	string resultado = encontrarSubcadenaMasLarga(palabras);
+	cout << "La subcadena mas larga es: " << resultado << endl;
 
-		}else{
-			cantPrefijos++;
-		}
-	}
-	
-	int indexPrefijoMasLargo = 0;
-	for (int i = 1; i < prefijos.size();i++){
-		if (prefijos[indexPrefijoMasLargo].size() < prefijos[i].size()){
-			indexPrefijoMasLargo = i;
-		}
-	}
-	
-	cout << endl << "La cadena repetida mas larga es " << prefijos[indexPrefijoMasLargo];
-	
-	
+	system("pause");
 	return 0;
 }
